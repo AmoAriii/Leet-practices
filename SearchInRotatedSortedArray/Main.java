0e55794cb918ff6b901371bb8a0dcb4f876e0a3f
Arian <amoisherebaby@gmail.com>
2026-08-13 19:34:12 +0200

    Solution

---
+++ SearchInRotatedSortedArray/Main.java
@@ -0,0 +1,12 @@
+package SearchInRotatedSortedArray;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Solution sol = new Solution();
+    int[] nums = {4,5,6,7,0,1,2};
+    System.out.println(sol.search(nums, 0));
+
+  }
+
+}
